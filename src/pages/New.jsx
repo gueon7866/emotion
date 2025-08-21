7b30e1fe78a72a5549bdc6b94b4472b21a0feba5
gueon7866 <lsg7866@gmail.com>
2025-08-21 09:09:53 +0900

    1
    1

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
+...
+import {useContext, useState} from 'react'
+import {DiaryDispatchContext} from '../App'
 const New = () => {
+
+  const {onCreate}=useContext(DiaryDispatchContext)
+
+  const onSubmit =(input)=>{
+    onCreate(
+      input.createdDate.getTime(),
+      input.emotionId,
+      input.content
+    )
+  }
+
   return (
-    <div>New</div>
+    <div>
+      <Header 
+      title={"새 일기 쓰기"}
+      leftChild={<Button text={"< 뒤로가기"}/>}
+      />
+      <Editor onSubmit={onSubmit}/>
+    </div>
   )
 }
 
